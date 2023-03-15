7d28a32b03f5ca728a85a91d5c3df77f10bd6d85
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-15 14:59:05 +0100

    [ADD] implemented interface

--- src/org/lessons/java/Cane.java
+++ src/org/lessons/java/Cane.java
@@ -1,8 +1,18 @@
 package org.lessons.java;
 
-public class Cane extends Animale{
+public class Cane extends Animale implements IAnimale{
 
     public Cane(String name) {
         super(name);
+    }
+
+    @Override
+    public void verso() {
+        System.out.println("BAU BAU!");
+    }
+
+    @Override
+    public void alimentazione() {
+        System.out.println("Crocchette");
     }
 }
