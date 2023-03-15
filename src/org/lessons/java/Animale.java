fcaa48c8ec36bd558504023a6d2504e8c80eee32
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-03-15 14:59:24 +0100

    [ADD] abstract methods

--- src/org/lessons/java/Animale.java
+++ src/org/lessons/java/Animale.java
@@ -4,10 +4,12 @@ public abstract class Animale {
 
     String name;
 
+    // COSTRUTTORE
     public Animale(String name) {
         this.name = name;
     }
 
+    // GETTERS AND SETTERS
     public String getName() {
         return name;
     }
@@ -15,4 +17,14 @@ public abstract class Animale {
     public void setName(String name) {
         this.name = name;
     }
+
+    // ABSTRACT METHODS
+
+    public void dorme(){
+        System.out.print("ZzZzZzz");
+    }
+
+    public abstract void verso();
+    public abstract void alimentazione();
+
 }
